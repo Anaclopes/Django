b0e8b430f46ee56ee32daae8fb9f2be97598d19b
Anaclopes <Anacl4udia13@gmail.com>
2025-08-28 20:22:58 -0300

    Feat: DRY and Model

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse #Biblioteca para definir diversas formas de responder a uma requisição
 
+dados = {
+    1: {
+        "nome": "Nebulosa de Carina",
+        "legenda": "webbtelecope.org / NASA / James Webb"
+    },
+
+    2:{
+        "nome": "Galaxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"
+    }
+}
+
 def index(request): #recebendo a requisição
-    return render(request, 'galeria/index.html') #O parâmetro sempre virá primeiro
+    #render permite retornar e enviar 
+    return render(request, 'galeria/index.html', {"cards": dados}) #O parâmetro sempre virá primeiro
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
